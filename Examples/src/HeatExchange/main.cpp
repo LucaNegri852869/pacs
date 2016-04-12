e36205e8f3f774188465e362e7c194071b6847c3
LucaNegri852869 <luca7.negri@mail.polimi.it>
2016-04-12 18:50:29 +0200

    Fixed the gnuplot part in main.cpp

--- Examples/src/HeatExchange/main.cpp
+++ Examples/src/HeatExchange/main.cpp
@@ -146,15 +146,14 @@ int main(int argc, char** argv)
        }
       break;
      case 1:
-     for(int m = 0; m<= M; m++)
-       {
-	 // \t writes a tab 
-         f<<m*h*L<<"\t"<<Te*(1.+theta[m])<<"\t"<<thetaa[m]<<endl;
-	 // An example of use of tie and tuples!
-         
-	 std::tie(coor[m],sol[m],exact[m])=
-	   std::make_tuple(m*h*L,Te*(1.+theta[m]),thetaa[m]);
-       }
+     for(int m = 0; m<= M; m++){
+       	std::tie(coor[m],sol[m],exact[m])=
+	std::make_tuple(m*h*L,Te*(1.+theta[m]),thetaa[m]);
+	}
+	// Using temporary files (another nice use of tie)
+	gp<<"plot"<<gp.file1d(std::tie(coor,sol))<<
+	"w lp title 'uh',"<< gp.file1d(std::tie(coor,exact))<<
+	"w l title 'uex'"<<std::endl;
      break;  
      case 2:
      cout<<"Result file: "<<out_file<<endl;
